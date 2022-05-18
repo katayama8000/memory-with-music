b321496e41452e9eff2373287472de0aff244ab3
katayama <tattu.0310@gmail.com>
2022-05-18 17:04:11 +0900

    ✨ itunesAPIで検索

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,40 @@
 import type { NextPage } from "next";
-import { Sample } from "@component/Sample";
+import { TextInput, Checkbox, Button, Group, Box } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import axios from "axios";
 
 const Home: NextPage = () => {
+  const form = useForm({
+    initialValues: {
+      music: "",
+    },
+  });
+
+  const handleSubmit = async (values) => {
+    console.log(values);
+    const { data } = await axios.get(
+      `//itunes.apple.com/search?term=${values}&country=jp&entity=musicVideo`
+    );
+    console.log(data);
+  };
+
   return (
     <div className="p-20">
       <h1 className="text-center italic hover:not-italic">memory with music</h1>
-      <Sample child="Hello Child" />
+      <Box sx={{ maxWidth: 300 }} mx="auto">
+        <form
+          onSubmit={form.onSubmit((values) => handleSubmit(values))}
+          className="mt-2 flex gap-x-2"
+        >
+          <TextInput
+            placeholder="searching for music"
+            {...form.getInputProps("music")}
+          />
+          <Button type="submit" color="cyan">
+            Serarch
+          </Button>
+        </form>
+      </Box>
     </div>
   );
 };
