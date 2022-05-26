44ca0cc83b27c41931db278409ce818dfb27ed5c
katayama <tattu.0310@gmail.com>
2022-05-26 16:52:59 +0900

    🔥

--- src/lib/$path.ts
+++ src/lib/$path.ts
@@ -1,12 +1,12 @@
 export const pagesPath = {
+  "form": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/form' as const, hash: url?.hash })
+  },
   "memories": {
     $url: (url?: { hash?: string }) => ({ pathname: '/memories' as const, hash: url?.hash })
   },
   "memoryArticle": {
     $url: (url?: { hash?: string }) => ({ pathname: '/memoryArticle' as const, hash: url?.hash })
-  },
-  "supa": {
-    $url: (url?: { hash?: string }) => ({ pathname: '/supa' as const, hash: url?.hash })
   },
   "test": {
     $url: (url?: { hash?: string }) => ({ pathname: '/test' as const, hash: url?.hash })
