0d5e901e6b15deafb18dba9455069cb140e91485
katayama <tattu.0310@gmail.com>
2022-06-21 23:05:03 +0900

    signUp

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,12 +1,68 @@
-import React from "react";
+import { NextPage } from "next";
+import { config } from "../lib/supabase/supabase";
+import { useForm } from "@mantine/form";
+import { TextInput, Button, Group, Box, PasswordInput } from "@mantine/core";
 
-const signup = () => {
+type IForm = {
+  email: string;
+  password: string;
+};
+
+const Signin: NextPage = () => {
+  const handleSignin = async (value: { email: string; password: string }) => {
+    console.log(value);
+    const { user, session, error } = await config.supabase.auth.signUp({
+      email: value.email,
+      password: value.password,
+    });
+
+    if (user) {
+      console.log(user);
+    }
+    if (session) {
+      console.log(session);
+    }
+    if (error) {
+      console.log(error);
+    }
+  };
+
+  const form = useForm({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+    },
+  });
   return (
-    <>
-      <div>signup</div>
-      <h1>SignUp</h1>
-    </>
+    <Box sx={{ maxWidth: 300 }} mx="auto">
+      <form onSubmit={form.onSubmit((values) => handleSignin(values))}>
+        <TextInput
+          required
+          label="Email"
+          placeholder="your@email.com"
+          {...form.getInputProps("email")}
+          className="my-4"
+        />
+
+        <PasswordInput
+          label="Password"
+          placeholder="Password"
+          {...form.getInputProps("password")}
+          className="my-4"
+        />
+
+        <Group position="center" mt="xl">
+          <Button type="submit" color="cyan">
+            SignUp
+          </Button>
+        </Group>
+      </form>
+    </Box>
   );
 };
 
-export default signup;
+export default Signin;
