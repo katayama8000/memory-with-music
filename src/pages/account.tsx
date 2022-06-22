ad3d7a306e644381fe5a62dbedccf65722efcd2b
katayama <tattu.0310@gmail.com>
2022-06-23 00:46:46 +0900

    アカウント画面 #20

---
+++ src/pages/account.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+const Account: NextPage = () => {
+  return (
+    <div>
+      <div>aa</div>
+    </div>
+  );
+};
+
+export default Account;
