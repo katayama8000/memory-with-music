083a5f9acb4c2eecc132e9ce6c4ac290443db871
katayama <tattu.0310@gmail.com>
2022-11-14 18:00:02 +0900

    feat: test

--- src/components/Article/ArticleCard.test.tsx
+++ src/components/Article/ArticleCard.test.tsx
@@ -4,10 +4,26 @@ import { ArticleCard, SkeletonCard } from './ArticleCard';
 
 describe('rendering', () => {
   it('should render SongCard', () => {
-    render(<ArticleCard image={'https://'} id={0} artist={'katayama'} song={'your song'} memory={'honoka'} />);
-    //screen.debug();
+    const ArticleMock = {
+      id: 1,
+      artist: 'katayama',
+      image: 'https://twitter.com/',
+      memory: 'honoka',
+      song: 'your song',
+    };
+    render(
+      <ArticleCard
+        image={'https://'}
+        id={ArticleMock.id}
+        artist={ArticleMock.artist}
+        song={ArticleMock.song}
+        memory={ArticleMock.memory}
+      />
+    );
     expect(screen.getAllByAltText('katayama')).toBeTruthy();
     expect(screen.getByText('your song')).toBeTruthy;
+    expect(screen.getByText('katayama')).toBeTruthy;
+    expect(screen.getByText('honoka')).toBeTruthy;
     screen.debug(screen.getByText('your song'));
   });
 
